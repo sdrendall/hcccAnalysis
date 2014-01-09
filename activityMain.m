8eba322eb8703522f6f60efd3374cf3a6306f306
Sam Rendall <sdrendall@gmail.com>
2014-01-09 17:24:24 -0500

    Commented some unwanted bits out.  Temporarally converted to a script for debugging purposes

--- activityMain.m
+++ activityMain.m
@@ -1,4 +1,4 @@
-function Conditions = activityMain
+%function Conditions = activityMain
 % Main Script for hccc behavior analysis 
 
 
@@ -68,29 +68,29 @@ for iCond = 1:length(Conditions)
 end
 return
 
-%----------------------------------------------------------------
-%----------------------------Plot Data---------------------------
-%----------------------------------------------------------------
-
-% Time Spent in ROI
-[Conditions(i).location.inROI, Conditions(i).location.notInROI] = catagorizeCentroids(Conditions(i).centroids, Conditions(i).roi);
-
-
-% Detect Activity
- % Calculate Centroid Displacement
-Conditions(i).displacement.raw = calculateCentroidDisplacement(Conditions(i).centroids);
- % Lowpass filter?
-Conditions(i).displacement.lp80 = smoothVector(Conditions(i).displacement.raw, 80);
- % Bin
-Conditions(i).displacement.bin27 = binVector(Conditions(i).displacement.raw, 27, 'mean'); 
-
-
-% Save Results
-[savefile, savepath] = uiputfile;
-save([savepath, savefile], 'Conditions');
-
-%figure, bar([length(inROI), length(notInROI)])
-%figure, plot(displacement)
+% %----------------------------------------------------------------
+% %----------------------------Plot Data---------------------------
+% %----------------------------------------------------------------
+% 
+% % Time Spent in ROI
+% [Conditions(i).location.inROI, Conditions(i).location.notInROI] = catagorizeCentroids(Conditions(i).centroids, Conditions(i).roi);
+% 
+% 
+% % Detect Activity
+%  % Calculate Centroid Displacement
+% Conditions(i).displacement.raw = calculateCentroidDisplacement(Conditions(i).centroids);
+%  % Lowpass filter?
+% Conditions(i).displacement.lp80 = smoothVector(Conditions(i).displacement.raw, 80);
+%  % Bin
+% Conditions(i).displacement.bin27 = binVector(Conditions(i).displacement.raw, 27, 'mean'); 
+% 
+% 
+% % Save Results
+% [savefile, savepath] = uiputfile;
+% save([savepath, savefile], 'Conditions');
+% 
+% %figure, bar([length(inROI), length(notInROI)])
+% %figure, plot(displacement)
 
 
 
