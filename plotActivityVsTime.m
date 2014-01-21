67872f82999d564cf38cbd19c97fe5559223b6a4
Sam Rendall <sdrendall@gmail.com>
2014-01-20 23:06:17 -0500

    Added missing blocks to Conditions.  New plot fcns

--- plotActivityVsTime.m
+++ plotActivityVsTime.m
@@ -2,7 +2,7 @@ function plotActivityVsTime(Conditions)
 
 
 %% Unpack Conditions
-%mice = extractMice(Conditions);
+mice = extractMice(Conditions);
 %blocks = extractBlocks(mice);
 
 
@@ -21,7 +21,7 @@ for iCond = 1:length(Conditions)
         currAx = gca;
         % plot activity data
         plot(Conditions(iCond).mouse(iMouse).allDisplacement.lp80, 'k', 'LineWidth', 1, 'color', [191/255, 0 ,0])
-        set(currAx, 'color', [.8 .8 .8])
+        %set(currAx, 'color', [.8 .8 .8])
         % add lines for start of day/night
         addVLine(currAx, Conditions(iCond).mouse(iMouse).day2night, [0, 0, 1])
         addVLine(currAx, Conditions(iCond).mouse(iMouse).night2day, [1, 111/255, 0])
@@ -35,9 +35,38 @@ for iCond = 1:length(Conditions)
     %export_fig(fullfile('~/Pictures/hcccAnalysis/activityVsTimePlots/', [Conditions(iCond).name, '_activityVsTime_1hrSmoothed']))
 end
 
+%% Plot Averages of Each Block over Time
+figure
+for iCond = 1:length(Conditions)
+    subplot(5, 1, iCond)
+    for iMouse = 1:Conditions(iCond).nMice
+        avgDisplacementVect{iMouse} = [Conditions(iCond).mouse(iMouse).tlBlock(:).avgDisplacement];
+    end
+    allData = aggregateVectors(avgDisplacementVect{:})';
+    maxBlocks = max([Conditions(iCond).mouse(:).nBlocks]);
+    % Remove NaNs from data
+    for iBk = 1:maxBlocks
+        currBlocksData = allData(:,iBk);
+        currBlocksData = currBlocksData(~isnan(currBlocksData));
+        avgData(iBk) = mean(currBlocksData);
+        stDev(iBk) = std(currBlocksData);
+    end        
+    errorbar(1:maxBlocks, avgData, stDev, 'k')
+    title(Conditions(iCond).name)
+    xlabel('Block Number')
+    ylabel('Average Pixels Displaced')
+    hold on
+    
+    % plot markers
+    longestMouse = find([Conditions(iCond).mouse(:).nBlocks] == maxBlocks, 1);
+    nightBlocks = strcmp({Conditions(iCond).mouse(longestMouse).tlBlock(:).timeOfDay}, 'night');
+    dayBlocks = strcmp({Conditions(iCond).mouse(longestMouse).tlBlock(:).timeOfDay}, 'day');
+    plot(find(nightBlocks), avgData(nightBlocks), 'o')
+    plot(find(dayBlocks), avgData(dayBlocks), 'yo')
+    clear avgDisplacementVect avgData stDev
+end  
 
 %% Internal Functions
-
 function addVLine(ax, x, clr)
 if ~exist('clr', 'var')
     clr = [0,0,1];
